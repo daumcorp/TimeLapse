84133b8112109fd598ef13bf3f48e26ef4110c45
이지홍 <ezhong@daumcorp.com>
2013-05-17 08:44:21 +0900

    they are working... it's not a holiday

--- cron-script.sh
+++ cron-script.sh
@@ -4,13 +4,13 @@ if test -z "$detected_device"; then
 	echo "Device not connected!"
 	exit 1
 fi
-today=`date +%m%d`
-holidays=" 0517 0606 0815 0918 0919 0920 1003 1009 1225 0101 0130 0131 0505 0506 "
-if [[ "$holidays" == *"$today"* ]]
-then
-	echo "it's holiday!"
-	exit 1
-fi
+#today=`date +%m%d`
+#holidays=" 0517 0606 0815 0918 0919 0920 1003 1009 1225 0101 0130 0131 0505 0506 "
+#if [[ "$holidays" == *"$today"* ]]
+#then
+#	echo "it's holiday!"
+#	exit 1
+#fi
 
 DIR=/home/pi/public_html/lapse
 mkdir -p $DIR/logs
